2a6216352027f4fa4b25769ade876a11fcedfd91
pchoanganh <hoang2005anh@gmail.com>
2023-10-23 09:07:50 +0700

    v1

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <header>
+    <div className="logo">Logo</div>
+    <div className="register">
+        <a href="">Dang ky</a>
+        <a href="">Dang nhap</a>
+    </div>
+</header>
+  )
+}
+
+export default Header
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='menu'>
+        <ul>
+            <li><a href="">Trang chu</a></li>
+            <li><a href="">Gioi thieu</a></li>
+            <li><a href="">San pham</a></li>
+            <li><a href="">Lien he</a></li>
+            <li><a href="">Tin tuc</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
